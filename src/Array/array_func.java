1e8cb711e368e32d06c191f26c7b569d2f19d839
abdelkarim elhaouil <karimwins177@gmail.com>
2025-02-05 20:13:50 +0100

    work on generalizing the arry_func class with help of generics

--- src/Array/array_func.java
+++ src/Array/array_func.java
@@ -1,37 +1,40 @@
 package Array;
-public class array_func {
-    public static void selectionSort(int[] arr) {
+
+import java.util.Arrays;
+
+public final class array_func{
+    public static <T extends Comparable<T>> void selectionSort(T[] arr) {
         for (int i = 0; i < arr.length; ++i) {
             int min = i;
             for (int j = i + 1; j < arr.length; ++j) {
-                if (arr[min] > arr[j])
+                if (arr[min].compareTo(arr[j]) > 0)
                     min = j;
             }
-            int tmp = arr[min];
+            T tmp = arr[min];
             arr[min] = arr[i];
             arr[i] = tmp;
         }
     }
-    public static void selectionSortDes(int[] arr) {
+    public <T extends Comparable<T>> void selectionSortDescending(T[] arr) {
         for (int i = 0; i < arr.length - 1; ++i) {
             int max = i;
             for (int j = i + 1; j < arr.length; ++j) {
-                if (arr[max] < arr[j])
+                if (arr[max].compareTo(arr[j]) < 0)
                     max = j;
             }
-            int tmp = arr[max];
+            T tmp = arr[max];
             arr[max] = arr[i];
             arr[i] = tmp;
         }
     }
     //--------------------------------------------------------//
-    public static void bubbleSort(int[] arr){
+    public <T extends Comparable<T>> void bubbleSort(T[] arr){
         boolean flag;
         do {
             flag = false;
             for(int i = 0; i < arr.length - 1; ++i){
-                if(arr[i] > arr[i+1]){
-                    int tmp = arr[i];
+                if(arr[i].compareTo(arr[i+1]) > 0){
+                    T tmp = arr[i];
                     arr[i] = arr[i+1];
                     arr[i+1] = tmp;
                     flag = true;
@@ -40,24 +43,26 @@ public class array_func {
         }while(flag);
     }
     //--------------------------------------------------------//
-    public static void insertionSort(int[] arr){
-        int key, j;
+    public static <T extends Comparable<T>> void insertionSort(T[] arr){
+        int j;
+        T key;
         for(int i = 1; i < arr.length; ++i){
             key = arr[i];
             j = i - 1;
-            while (j >= 0 && key < arr[j]){
+            while (j >= 0 && key.compareTo(arr[j]) < 0){
                 arr[j+1] = arr[j];
                 j--;
             }
             arr[j+1] = key;
         }
     }
-    public static void insertionSortDes(int[] arr){
-        int key, j;
+    public static <T extends Comparable<T>> void insertionSortDescending(T[] arr){
+        int j;
+        T key;
         for(int i = 1; i < arr.length; ++i){
             key = arr[i];
             j = i - 1;
-            while (j >= 0 && key > arr[j]){
+            while (j >= 0 && key.compareTo(arr[j]) > 0){
                 arr[j+1] = arr[j];
                 j--;
             }
@@ -65,14 +70,14 @@ public class array_func {
         }
     }
     //--------------------------------------------------------//
-    public static void merge(int[] values, int start, int mid, int end){
-        int[] sorted = new int[values.length];
+    public static <T extends Comparable<T>> void merge(T[] values, int start, int mid, int end){
+        T[] sorted = Arrays.copyOf(values, values.length);
         for(int i = start; i < (end-start) - 1; ++i){
             if(mid >= end)
                 sorted[start++] = values[start++];
             else if (start >= mid)
                 sorted[mid++] = values[mid++];
-            else if(values[start] < values[mid])
+            else if(values[start].compareTo(values[mid]) < 0)
                 sorted[start++] = values[start++];
             else
                 sorted[mid++] = values[mid++];
